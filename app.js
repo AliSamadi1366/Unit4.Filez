ae4af37469992ca4334e45b1e384b0e79ce86d82
Ali Samadi <alisamadi.096@gmail.com>
2025-05-02 00:54:58 -0500

    define router for filesrouter and folderrouter

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 import express from "express";
+import filesRouter from "#api/Filez";
+import foldersRouter from "#api/Folders";
 const app = express();
 export default app;
+app.use(express.json());
+
+
+app.use("/files", filesRouter);
+app.use("/folders", foldersRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong :(");
+});
